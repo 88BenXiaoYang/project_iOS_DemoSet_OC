f8db5cdae913884b01ba6eef87bed17c5b8a47b6
bianyong <bianyong@yanxiu.com>
2018-11-29 14:06:51 +0800

    add home page

---
+++ project_iOS_DemoSet_OC/ProjectFrame/HomePage/BYHomePageViewController.h
@@ -0,0 +1,17 @@
+//
+//  BYHomePageViewController.h
+//  project_iOS_DemoSet_OC
+//
+//  Created by srt on 2018/11/29.
+//  Copyright © 2018 by. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BYHomePageViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ project_iOS_DemoSet_OC/ProjectFrame/HomePage/BYHomePageViewController.m
@@ -0,0 +1,23 @@
+//
+//  BYHomePageViewController.m
+//  project_iOS_DemoSet_OC
+//
+//  Created by srt on 2018/11/29.
+//  Copyright © 2018 by. All rights reserved.
+//
+
+#import "BYHomePageViewController.h"
+
+@interface BYHomePageViewController ()
+
+@end
+
+@implementation BYHomePageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.navigationItem.title = @"~~~";
+}
+
+@end
