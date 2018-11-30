783be17ef7d94b7d824699278be5f277516c51a4
bianyong <bianyong@yanxiu.com>
2018-11-30 10:16:25 +0800

    add slideView

---
+++ project_iOS_DemoSet_OC/View/slideView/BYSlideView.h
@@ -0,0 +1,17 @@
+//
+//  BYSlideView.h
+//  project_iOS_DemoSet_OC
+//
+//  Created by srt on 2018/11/30.
+//  Copyright © 2018 by. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BYSlideView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ project_iOS_DemoSet_OC/View/slideView/BYSlideView.m
@@ -0,0 +1,21 @@
+//
+//  BYSlideView.m
+//  project_iOS_DemoSet_OC
+//
+//  Created by srt on 2018/11/30.
+//  Copyright © 2018 by. All rights reserved.
+//
+
+#import "BYSlideView.h"
+
+@implementation BYSlideView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
