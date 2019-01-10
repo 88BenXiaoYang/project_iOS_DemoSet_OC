be91a7c8c20fa95d0485e1d807fc86015f9358e0
bianyong <bianyong@yanxiu.com>
2019-01-10 23:39:25 +0800

    modify BYTools

--- project_iOS_DemoSet_OC/Common/BYTools.m
+++ project_iOS_DemoSet_OC/Common/BYTools.m
@@ -34,6 +34,54 @@ static BYTools *_instance = nil;
     return _instance;
 }
 
++ (NSString *)timeStrFrom:(double)timeStmp{
+    NSDate* date = [NSDate dateWithTimeIntervalSince1970:timeStmp];
+    //    一分钟内  40秒前或刚刚
+    //    一小时内 XX分钟前
+    //    一小时到24小时  今天XX：XX或XX小时前
+    //    24小时前  显示日期 比如6-25 13：25
+    
+    const NSTimeInterval min  = 60.0;
+    const NSTimeInterval hour = 60.0 * 60;
+    const NSTimeInterval day  = 60.0 * 60 * 24;
+    
+    NSTimeInterval interval = [date timeIntervalSinceNow];
+    interval = ABS(interval);
+    
+    NSDateFormatter* fm = [[NSDateFormatter alloc] init];
+    NSString* result = @"";
+    
+    //分情况
+    NSDate* now = [NSDate date];
+    [fm setDateFormat:@"yyyyMMdd"];
+    NSString* todayStr = [fm stringFromDate:now];
+    NSDate* today = [fm dateFromString:todayStr];//今天00：00
+    
+    if ([date timeIntervalSinceDate:today] > 0){
+        //今天
+        if (interval < min){
+            result =  @"刚刚";
+        }
+        else    if(interval >= min
+                   && interval < hour){
+            interval /= min;
+            result = [NSString stringWithFormat:@"%d分钟前", (int)interval];
+        }
+        else    if(interval >= hour
+                   && interval < day){
+            [fm setDateFormat:@"今天HH:mm"];
+            result = [fm stringFromDate:date];
+        }
+    }
+    else {
+        //昨天之前
+        [fm setDateFormat:@"MM-dd HH:mm"];
+        result = [fm stringFromDate:date];
+    }
+    
+    return result;
+}
+
 - (void)prepare {
     _screenWidth = [UIScreen mainScreen].bounds.size.width;
     _screenHeight = [UIScreen mainScreen].bounds.size.height;
